2d8a593b0f8ef91f4317bdd8013f5063ea337374
graciany <gr4ciany@gmail.com>
2026-03-11 21:10:05 -0300

    http methods

--- Node/course-node-with-nest/src/tasks/tasks.controller.ts
+++ Node/course-node-with-nest/src/tasks/tasks.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get, Patch, Post, Put } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 
 @Controller({})
 export class TasksController {
-    constructor (private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) { }
 
     @Get('/tasks')
     getAllTasks() {
         return this.tasksService.getTasks();
+    }
+
+    @Post('/tasks')
+    createTask() {
+        return this.tasksService.createTask();
+    }
+
+    @Put('/tasks')
+    editTasks() {
+        return this.tasksService.editTasks();
+    }
+
+    @Patch('/tasks')
+    editTask() {
+        return this.tasksService.editTask();
+    }
+
+    @Delete('/tasks')
+    deleteTask() {
+        return this.tasksService.deleteTask();
     }
 }
--- Node/course-node-with-nest/src/tasks/tasks.service.ts
+++ Node/course-node-with-nest/src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
     getTasks() {
         return ['Task 1', 'Task 2', 'Task 3'];
     }
+
+    createTask() {
+        return 'Task created';
+    }
+
+    editTask() {
+        return 'Task edited';
+    }
+
+    editTasks() {
+        return 'Tasks edited';
+    }
+
+    deleteTask() {
+        return 'Task deleted';
+    }
 }
